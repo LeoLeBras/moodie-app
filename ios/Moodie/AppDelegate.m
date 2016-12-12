e024c6b79fbfdd44bc2a1f51aa7fd2f41c5cf7f3
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-12-12 14:52:19 +0100

    feat(ios): add app icon  + launchscreen

--- ios/Moodie/AppDelegate.m
+++ ios/Moodie/AppDelegate.m
@@ -8,6 +8,7 @@
  */
 
 #import "AppDelegate.h"
+#import "RCTUtils.h"
 
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
@@ -25,6 +26,30 @@
                                                       moduleName:@"Moodie"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+
+  // Get launch image
+  NSString *launchImageName = nil;
+  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+    CGFloat height = MAX(RCTScreenSize().width, RCTScreenSize().height);
+  if (height == 480) launchImageName = @"LaunchImage-700@2x.png"; // iPhone 4/4s
+  else if (height == 568) launchImageName = @"LaunchImage-700-568h@2x.png"; // iPhone 5/5s
+  else if (height == 667) launchImageName = @"LaunchImage-800-667h@2x.png"; // iPhone 6
+  else if (height == 736) launchImageName = @"LaunchImage-800-Portrait-736h@3x.png"; // iPhone 6+
+  } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+    CGFloat scale = RCTScreenScale();
+  if (scale == 1) launchImageName = @"LaunchImage-700-Portrait~ipad.png"; // iPad
+  else if (scale == 2) launchImageName = @"LaunchImage-700-Portrait@2x~ipad.png"; // Retina iPad
+  }
+
+  // Create loading view
+  UIImage *image = [UIImage imageNamed:launchImageName];
+  if (image) {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:(CGRect){CGPointZero, RCTScreenSize()}];
+    imageView.contentMode = UIViewContentModeBottom;
+    imageView.image = image;
+    rootView.loadingView = imageView;
+  }
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
